39872a08f0c0d710784ce1d086028c1f864cf368
programacion <programacion@evolis3d.com>
2021-04-20 15:06:28 +0200

    1st commit
    Subida del proyecto con los materiales necesarios
    
    TO-DO:  crear fichero de 3-4 charlas, serializar en ScriptableObject, y crear una CoRoutine para mostrarlas con timers. Debe usar 2 transforms para los NPC.

---
+++ Assets/Scripts/GAME.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GAME : MonoBehaviour
+{
+    public TMP_Text capto;
+    public Canvas cv;
+
+    private void Start()
+    {
+        cv.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            capto.text = dialogo();
+            StartCoroutine(showDialog());
+        }
+    }
+
+    private string dialogo()
+    {
+        List<string> textos = new List<string>();
+        textos.Add("Hola guapo!");
+        textos.Add("Ostras! Creo que me he equivocado de juego...");
+        textos.Add("Pulsa cualquier tecla para empezar.");
+        textos.Add("En ocasiones, veo pixels...");
+
+        var po = Random.Range(0, 4);
+        return textos[po];
+    }
+
+    private IEnumerator showDialog()
+    {
+        cv.enabled = true;
+        yield return new WaitForSeconds(1f);
+        cv.enabled = false;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Dialogos
- Ejemplo de sistema de conversaciones entre personajes
+# Diálogos2D
+
+### Ejemplo de secuencia de diálogos entre personajes
+
+by **Carlos Lecina**
+
+------
+
+Es un proyecto de ejemplo de un Videojuego 2D Pixel Art de plataformas para aprender cómo funcionan las secuencias de diálogos automáticas entre personajes.
+
+La base del Proyecto usa un escenario Tilemap y lleva una cámara Pixel Perfect implementada.
+
+El objetivo es que al desencadenar un evento, se pongan a hablar 2 personajes con conversaciones aleatorias sacadas de una lista de ellas. (juego de referencia: IDLE APOCALYPSE)
+
+------
+
